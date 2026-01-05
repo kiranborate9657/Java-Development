a6cf8a994f33ce285d6518946dac7c88e9075eb6
Kiran Borate <boratek844@gmail.com>
2026-01-05 19:24:10 +0530

    pattern printing

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Printing the pattern:");
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=5+2;j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
