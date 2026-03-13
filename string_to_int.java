ecdd0e80c540d166b681bda1071f5baea40cbd13
Kiran Borate <boratek844@gmail.com>
2026-03-13 23:06:20 +0530

    Create string_to_int.java

---
+++ string_to_int.java
@@ -0,0 +1,9 @@
+public class string_to_int {
+    public static void main(String[] args) {
+        String str="12345";
+        int num=Integer.parseInt(str);
+        System.out.println("String: " + str);
+        System.out.println("Integer: " + num);
+        
+    }
+}
