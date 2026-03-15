59d5fe274fc1a8c4624259b4a8d3c1f5968ef8cc
Kiran Borate <boratek844@gmail.com>
2026-03-15 23:29:03 +0530

    Create Multiple_catch.java

---
+++ Multiple_catch.java
@@ -0,0 +1,16 @@
+public class MultipleCatch {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10/0;
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception occurred");
+        }
+
+        catch (Exception e) {
+            System.out.println("General Exception");
+        }
+    }
+}
