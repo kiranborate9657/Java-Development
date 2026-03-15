33b0b9004f320e22f35bc27d56d0dd74ce05e895
Kiran Borate <boratek844@gmail.com>
2026-03-15 23:28:24 +0530

    Create Exception_finally.java

---
+++ Exception_finally.java
@@ -0,0 +1,15 @@
+public class FinallyExample {
+    public static void main(String[] args) {
+
+        try {
+            int arr[] = {1,2,3};
+            System.out.println(arr[5]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is invalid");
+        }
+        finally {
+            System.out.println("This block always executes");
+        }
+    }
+}
