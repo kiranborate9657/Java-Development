f31448eeba57a31620dce8046d422354e3d46ce2
Kiran Borate <boratek844@gmail.com>
2026-03-16 22:45:36 +0530

    Create Methodoverloading.java

---
+++ Methodoverloading.java
@@ -0,0 +1,19 @@
+class Addition {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class PolymorphismDemo {
+    public static void main(String[] args) {
+        Addition obj = new Addition();
+
+        System.out.println(obj.add(5, 10));
+        System.out.println(obj.add(5.5, 3.2));
+    }
+}
