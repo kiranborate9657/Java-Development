2542e93a8088601d685eb6dc5d956927c74e8f8d
Kiran Borate <boratek844@gmail.com>
2026-03-20 21:56:42 +0530

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+        try {
+            int a = 10, b = 0;
+            int c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Program finished");
+        }
+    }
+}
