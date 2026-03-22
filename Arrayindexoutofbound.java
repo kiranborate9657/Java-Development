2685ef1db399d654ba2958647bffcff246fc06c0
Kiran Borate <boratek844@gmail.com>
2026-03-22 23:06:55 +0530

    Create Arrayindexoutofbound.java

---
+++ Arrayindexoutofbound.java
@@ -0,0 +1,14 @@
+public class ArrayIndexExample {
+    public static void main(String[] args) {
+
+        int arr[] = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.println(arr[7]);
+        }
+
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Invalid array index.");
+        }
+    }
+}
